c328c1237a4a7bd5fac959f53322ced081613b3a
fouquetp <peter.fouquet@cgi.com>
2023-06-14 08:03:22 +0200

    Release 3.0.0 * Update to edison 3

--- src/main/java/de/otto/edison/hmac/HmacConfiguration.java
+++ src/main/java/de/otto/edison/hmac/HmacConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class HmacConfiguration {
